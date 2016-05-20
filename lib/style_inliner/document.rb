3a3b858249a7f960ed5ef3e2c4f66de1fbfcfea7
r7kamura <r7kamura@gmail.com>
2016-05-21 05:08:42 +0900

    Prevent styling head element and its child elements

--- lib/style_inliner/document.rb
+++ lib/style_inliner/document.rb
@@ -2,7 +2,6 @@ require "css_parser"
 require "nokogiri"
 require "style_inliner/declaration_block"
 require "style_inliner/rule_set"
-require "style_inliner/selector"
 
 module StyleInliner
   class Document
@@ -20,6 +19,29 @@ module StyleInliner
     end
 
     private
+
+    # @param selector [Stirng]
+    # @param declarations [String]
+    # @param media_types [Array<Symbol>]
+    def add_unmergeable_rule_set(selector, declarations, media_types)
+      css_parser_for_unmergeable_rules.add_rule_set!(
+        ::CssParser::RuleSet.new(selector, declarations),
+        media_types,
+      )
+    end
+
+    # @param node [Nokogiri::XML::Node]
+    # @return [false, true]
+    def check_node_stylability(node)
+      node.element? && node.name != "head" && node.parent.name != "head"
+    end
+
+    # @todo
+    # @param selector [String]
+    # @return [false, true]
+    def check_selector_mergeability(selector)
+      true
+    end
 
     # @return [CssParser::Parser]
     def css_parser_for_mergeable_rules
@@ -56,19 +78,23 @@ module StyleInliner
 
     def merge_styles_into_each_element
       css_parser_for_mergeable_rules.each_selector(:all) do |selector, declarations, specificity, media_types|
-        if Selector.new(selector).unmergeable?
-          css_parser_for_unmergeable_rules.add_rule_set!(
-            ::CssParser::RuleSet.new(selector, declarations),
-            media_types,
-          )
-        else
+        if check_selector_mergeability(selector)
           root.search(selector).each do |node|
-            if node.element?
-              node["style"] = "#{node['style']} #{RuleSet.new(declarations: declarations, specificity: specificity).encode}"
+            if check_node_stylability(node)
+              push_encoded_rule_set_into_style_attribute(node, declarations, specificity)
             end
           end
+        else
+          add_unmergeable_rule_set(selector, declarations, media_types)
         end
       end
+    end
+
+    # @param node [Nokogiri::XML::Node]
+    # @param declarations [String]
+    # @param specificity [Integer]
+    def push_encoded_rule_set_into_style_attribute(node, declarations, specificity)
+      node["style"] = "#{node['style']} #{RuleSet.new(declarations: declarations, specificity: specificity).encode}"
     end
 
     # @return [Nokogiri::XML::Node]
--- spec/style_inliner/inliner_spec.rb
+++ spec/style_inliner/inliner_spec.rb
@@ -10,35 +10,70 @@ RSpec.describe StyleInliner::Inliner do
       inliner.call(html).to_s.gsub(/^ +\n/, "")
     end
 
-    let(:html) do
-      <<-EOS.strip_heredoc
-        <!DOCTYPE html>
-        <html>
-          <head>
-            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-            <style>
-              body {
-                background: red;
-              }
-            </style>
-          </head>
-          <body>
-          </body>
-        </html>
-      EOS
+    context "with style element in head element" do
+      let(:html) do
+        <<-EOS.strip_heredoc
+          <!DOCTYPE html>
+          <html>
+            <head>
+              <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+              <style>
+                body {
+                  color: red;
+                }
+              </style>
+            </head>
+            <body>
+            </body>
+          </html>
+        EOS
+      end
+
+      it "inlines style element into style attributes" do
+        is_expected.to eq <<-EOS.strip_heredoc
+          <!DOCTYPE html>
+          <html>
+            <head>
+              <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+            </head>
+            <body style="color: red;">
+            </body>
+          </html>
+        EOS
+      end
     end
 
-    it "inlines style element into style attributes" do
-      is_expected.to eq <<-EOS.strip_heredoc
-        <!DOCTYPE html>
-        <html>
-          <head>
-            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-          </head>
-          <body style="background-color: red;">
-          </body>
-        </html>
-      EOS
+    context "with style with wildcard element selector" do
+      let(:html) do
+        <<-EOS.strip_heredoc
+          <!DOCTYPE html>
+          <html>
+            <head>
+              <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+              <style>
+                * {
+                  color: red;
+                }
+              </style>
+            </head>
+            <body>
+            </body>
+          </html>
+        EOS
+      end
+
+      it "does not apply styles to elements within head element" do
+        is_expected.to eq <<-EOS.strip_heredoc
+          <!DOCTYPE html>
+          <html style="color: red;">
+            <head>
+              <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+            </head>
+            <body style="color: red;">
+            </body>
+          </html>
+        EOS
+      end
     end
   end
 end
